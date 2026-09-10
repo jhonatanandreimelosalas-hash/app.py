74123c68a35de9d383fd579b84ad58e5cc1e1eee
jhonatanandreimelosalas-hash <jhonatanandreimelosalas@gmail.com>
2026-09-09 22:29:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -755,7 +755,7 @@ elif menu == "8. Reporte Final":
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
 
-elif menu == "10. Reporte Ejecutivo PDF":
+elif menu == "9. Reporte Ejecutivo PDF":
     st.markdown('<p class="main-header">🖨️ Generador de Reporte Ejecutivo PDF</p>', unsafe_allow_html=True)
     st.markdown('<p class="sub-header">Genera y descarga instantáneamente el informe formal en PDF.</p>', unsafe_allow_html=True)
     st.markdown("---")
